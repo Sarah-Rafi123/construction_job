647ce64f60c127345fd6a4fadea15d7a6f34c8c3
user <user@users-mbp.lan>
2025-05-19 18:52:34 +0500

    password validation change

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -109,8 +109,8 @@ export default function LoginPage() {
     if (!formData.password) {
       newErrors.password = "Password is required"
       isValid = false
-    } else if (formData.password.length < 8) {
-      newErrors.password = "Password must be at least 8 characters long"
+    } else if (formData.password.length < 6) {
+      newErrors.password = "Password must be at least 6 characters long"
       isValid = false
     }
 
